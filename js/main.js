e66dbadb68d5d8159ae77be5e4d857d4cf60d6b8
Squishy123 <christian.gnaw@gmail.com>
2018-12-12 18:33:12 -0500

    Added flexbox integration for animations

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 class ColorSquare extends DOMActor {
     constructor(element, properties) {
         super(element, properties);
-        this.setStyles({ "background-color": "cornflowerblue" , "transition": "background-color 0.3s ease"})
+        this.setStyles({ "background-color": "cornflowerblue", "transition": "background-color 0.3s ease" })
     }
 
     render() {
@@ -12,11 +12,19 @@ class ColorSquare extends DOMActor {
 
 
 let stage = new ColorSquare(document.querySelector('#stage'), { width: '100%', height: '100%', updateTicksPerSecond: 1, renderTicksPerSecond: 1 });
+stage.setStyles({ display: "flex", "flex-direction": 'row', "flex-wrap": 'wrap' });
 
-let box = new ColorSquare(null, { width: '50%', height: '50%', updateTicksPerSecond: 1, renderTicksPerSecond: 5 });
-box.setStyles({ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' });
+let tiles = [];
+
+for (let x = 0; x < 4; x++) {
+    for (let y = 0; y < 4; y++) {
+        let box = new ColorSquare(null, { width: '25%', height: '25%', updateTicksPerSecond: 1, renderTicksPerSecond: 5 });
+        box.setStyles({ 'opacity': 0.2});
+        tiles.push(box);
+        stage.addChild(box);
+    }
+}
 
 
-stage.addChild(box);
 stage.start();
 //box.start();
